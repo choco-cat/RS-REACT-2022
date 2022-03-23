d0724a524cce67f7a4a9a93608bdb00910e3699e
Natalia Panfilenka <choco-cat@mail.ru>
2022-03-23 09:30:44 +0300

    feat: styling the search field

--- react_components/src/components/search.tsx
+++ react_components/src/components/search.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 export default class extends React.Component {
   render() {
     return (
-      <form action="/" method="get">
-        <input type="text" placeholder="Search..." name="search" />
-        <button type="submit">Search</button>
+      <form className="form-wrapper" action="/" method="get">
+        <input id="search" type="text" placeholder="Search..." name="search" />
+        <input type="submit" value="Go!" id="submit" />
       </form>
     );
   }
