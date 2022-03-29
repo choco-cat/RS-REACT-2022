9708c969a5fdeae58f64ba497259d145c359c815
Natalia Panfilenka <choco-cat@mail.ru>
2022-03-29 09:20:41 +0300

    feat: first input

--- react_forms/src/components/form.tsx
+++ react_forms/src/components/form.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
 
 export default class extends React.Component<object> {
-  private readonly inputRef: React.RefObject<HTMLInputElement>;
+  private readonly inputRefFirstname: React.RefObject<HTMLInputElement>;
+  private readonly inputRefLastname: React.RefObject<HTMLInputElement>;
   constructor(props: object) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.inputRef = React.createRef();
+    this.inputRefFirstname = React.createRef();
+    this.inputRefLastname = React.createRef();
   }
 
   handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    console.log('Submit form!');
+    console.log('Validation=' + this.inputRefFirstname.current!.value);
+    console.log('Validation=' + this.inputRefLastname.current!.value);
     e.preventDefault();
-    console.log('this.input value=' + this.inputRef.current!.value);
+  }
+
+  validationForm(e: React.MouseEvent<HTMLInputElement>) {
+    // console.log('Validation=' + this.inputRefFirstname.current!.value);
+    /* console.log('Validation=' + this.inputRefLastname.current!.value);
+    e.preventDefault(); */
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Имя:
-          <input type="text" ref={this.inputRef} />
+          First Name:
+          <input type="text" ref={this.inputRefFirstname} />
         </label>
-        <input type="submit" value="Отправить" />
+        <label>
+          Last Name:
+          <input type="text" ref={this.inputRefLastname} />
+        </label>
+        <input type="submit" value="Отправить" onClick={this.validationForm} />
       </form>
     );
   }
