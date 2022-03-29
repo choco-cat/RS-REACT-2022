e49c0a1e68116001885c2a236b2b3a1c3a5f488c
Natalia Panfilenka <choco-cat@mail.ru>
2022-03-29 10:31:58 +0300

    feat: validation for button click

--- react_forms/src/components/form.tsx
+++ react_forms/src/components/form.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
+type formElements = { ref: React.RefObject<HTMLInputElement>; rule: string };
 
 export default class extends React.Component<object> {
-  private readonly inputRefFirstname: React.RefObject<HTMLInputElement>;
-  private readonly inputRefLastname: React.RefObject<HTMLInputElement>;
+  private readonly inputRefFirstName: formElements;
+  private readonly inputRefLastName: formElements;
+  private readonly inputRefDate: formElements;
   constructor(props: object) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.inputRefFirstname = React.createRef();
-    this.inputRefLastname = React.createRef();
+    this.inputRefFirstName = { ref: React.createRef(), rule: 'alpha' };
+    this.inputRefLastName = { ref: React.createRef(), rule: 'alpha' };
+    this.inputRefDate = { ref: React.createRef(), rule: 'date' };
+    this.validationForm.bind(this);
   }
 
   handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     console.log('Submit form!');
-    console.log('Validation=' + this.inputRefFirstname.current!.value);
-    console.log('Validation=' + this.inputRefLastname.current!.value);
     e.preventDefault();
   }
 
   validationForm(e: React.MouseEvent<HTMLInputElement>) {
-    // console.log('Validation=' + this.inputRefFirstname.current!.value);
-    /* console.log('Validation=' + this.inputRefLastname.current!.value);
-    e.preventDefault(); */
+    console.log('onClick');
+    console.log('validate inputRefFirstName =' + this.inputRefFirstName.ref.current!.value);
+    console.log('validate inputRefLastName=' + this.inputRefLastName.ref.current!.value);
   }
 
   render() {
@@ -28,13 +30,21 @@ export default class extends React.Component<object> {
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" ref={this.inputRefFirstname} />
+          <input type="text" ref={this.inputRefFirstName.ref} />
         </label>
         <label>
           Last Name:
-          <input type="text" ref={this.inputRefLastname} />
+          <input type="text" ref={this.inputRefLastName.ref} />
         </label>
-        <input type="submit" value="Отправить" onClick={this.validationForm} />
+        <label>
+          Date:
+          <input type="text" ref={this.inputRefDate.ref} />
+        </label>
+        <input
+          type="submit"
+          value="Отправить"
+          onClick={(e: React.MouseEvent<HTMLInputElement>) => this.validationForm(e)}
+        />
       </form>
     );
   }
